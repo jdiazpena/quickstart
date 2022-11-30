48b1fcc6332cc04c9f0cdd5350851efa97b1c186
Michael Hirsch <scivision@users.noreply.github.com>
2022-11-29 20:42:42 -0500

    SCC MPI broken

--- gcc.sh
+++ gcc.sh
@@ -4,10 +4,9 @@ module load cmake
 
 unset MKLROOT
 # in case used Intel compiler previously
+# preinstalled MPI is broken on this system
+module unload openmpi
 
 # HPC often have some compiler versions that are broken.
 # here's one we know works.
 module load gcc/12.2.0
-
-# an MPI library compatible with the selected compiler.
-module load openmpi/3.1.4
