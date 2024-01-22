9871f9847df60acdaf5b5f74ac1458953ff5457c
scivision <scivision@users.noreply.github.com>
2024-01-22 15:15:10 -0500

    hpc openmpi working again

--- gcc.sh
+++ gcc.sh
@@ -1,11 +1,12 @@
-module load git
+module load git/2.31.1
 module load python3
 module load cmake
 
 unset MKLROOT
 # in case used Intel compiler previously
-# preinstalled MPI is broken on this system
-module unload openmpi
+
+# if preinstalled MPI doesn't work, need to build OpenMPI per Readme
+module load openmpi/4.1.5
 
 # HPC often have some compiler versions that are broken.
 # here's one we know works.
